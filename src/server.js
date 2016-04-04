2392b330e0400bd8668cbf74c0ee28f725aee451
Chris Rittelmeyer <github.10.iamchris@spamgourmet.com>
2016-04-03 21:25:22 -0400

    Oops, typos

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ export default function startServer(store) {
   const io = new Server().attach(8090);
 
   store.subscribe(
-    () => io.emit('state', state.getStore().toJS())
+    () => io.emit('state', store.getState().toJS())
   );
 
   io.on('connection', (socket) => {
-    socket.emit('state', state.getStore().toJS())
+    socket.emit('state', store.getState().toJS())
     socket.on('action', store.dispatch.bind(store));
   });
 }
